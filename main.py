103dbdfb19a685becdfec8630a741eaf881468df
Ravel Momo <83598531+Notho-freedom@users.noreply.github.com>
2024-04-22 20:26:31 +0300

    news updates

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
-from V.test2 import AutoCompleterCodeEditor
+from V.code_editor import AutoCompleterCodeEditor
 from V.vstheme import SyntaxHighlighter
 
 class MainWindow(QMainWindow):
